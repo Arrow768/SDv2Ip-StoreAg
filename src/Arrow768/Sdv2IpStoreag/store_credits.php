eeaaf6e81e9647e187b4c69243c9d3d1f62a46fb
Werner 'Arrow768 <arrow768@sourcedonates.com>
2014-08-16 16:07:07 +0200

    Changed the Provider to handle store credits

--- src/Arrow768/Sdv2IpStoreag/store_credits.php
+++ src/Arrow768/Sdv2IpStoreag/store_credits.php
@@ -5,15 +5,14 @@ namespace Arrow768\Sdv2IpStoreag;
 /**
  * SDv2 Items Provider for Alongubs Store Plugin
  * 
- * A SDv2 Items Provider for Alongubkins Store Plugin
- * Contains the required functions that are used by SDv2
+ * Assigns Store Credits to Users of Alongubkins Store Plugin
  * 
  *  @package    SDv2IP-Paypal
  *  @author     Werner Maisl
  *  @copyright  (c) 2013-2014 - Werner Maisl
  */
 
-class items_store
+class store_credits
 {
     
     function add_item($sd_user_id,$sd_item_id)
