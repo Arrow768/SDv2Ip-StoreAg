79d1a6ae1060f00b0ac79f078d539c1e4fdf6bbe
Werner 'Arrow768 <arrow768@sourcedonates.com>
2014-08-16 22:08:45 +0200

    Updates store_credits

--- src/Arrow768/Sdv2IpStoreag/store_credits.php
+++ src/Arrow768/Sdv2IpStoreag/store_credits.php
@@ -11,36 +11,95 @@ namespace Arrow768\Sdv2IpStoreag;
  *  @author     Werner Maisl
  *  @copyright  (c) 2013-2014 - Werner Maisl
  */
-
 class store_credits
 {
-    
-    function add_item($sd_user,$sd_user_infos,$sd_item_handler_params)
+
+    function add_item($sd_user, $sd_user_infos, $sd_item_handler_params)
     {
         \Log::info("Store Items Provider - Store Credits called");
-        \Log::info("User Mail:".$sd_user->email);
-        \Log::info("Credits:".$sd_item_handler_params->credits);
-        
-        $steamid = "";
-        foreach($sd_user_infos as $sd_user_info)
+        \Log::info("User Mail:" . $sd_user->email);
+        \Log::info("Credits:" . $sd_item_handler_params->credits);
+
+        $steamid64 = "";
+        foreach ($sd_user_infos as $sd_user_info)
         {
-            if($sd_user_info->type == "steamid")
+            if ($sd_user_info->type == "steamid")
             {
-                $steamid = $sd_user_info->value;
+                $steamid64 = $sd_user_info->value;
             }
-            
-            \Log::info("User Info Type:".$sd_user_info->type);
-            \Log::info("User Info Value:".$sd_user_info->value);
+
+            \Log::info("User Info Type:" . $sd_user_info->type);
+            \Log::info("User Info Value:" . $sd_user_info->value);
         }
-        \Log::info("Steam:".$steamid);
+        \Log::info("Steamid64:" . $steamid64);
         
+        $steamid = $this->community_to_steamid($steamid64);
+        \Log::info("Steamid:". $steamid);
         
-        
+        $store_auth = $this->steamid_to_auth($steamid);
+        \Log::info("Auth:". $store_auth);
     }
-    
-    function remove_item($sd_user,$sd_user_infos,$sd_user_params)
+
+    function remove_item($sd_user, $sd_user_infos, $sd_user_params)
     {
         
     }
-    
+
+    private function steamid_to_auth($steamid)
+    {
+        //from https://forums.alliedmods.net/showpost.php?p=1890083&postcount=234
+        $toks = explode(":", $steamid);
+        $odd = (int) $toks[1];
+        $halfAID = (int) $toks[2];
+
+        return ($halfAID * 2) + $odd;
+    }
+
+    private function auth_to_steamid($authid)
+    {
+        $steam = array();
+        $steam[0] = "STEAM_0";
+
+        if ($authid % 2 == 0)
+        {
+            $steam[1] = 0;
+        }
+        else
+        {
+            $steam[1] = 1;
+            $authid -= 1;
+        }
+        $steam[2] = $authid / 2;
+        return $steam[0] . ":" . $steam[1] . ":" . $steam[2];
+    }
+
+    private function steamid_to_community($steamid)
+    {
+        $parts = explode(':', str_replace('STEAM_', '', $steamid));
+
+        $result = bcadd(bcadd('76561197960265728', $parts['1']), bcmul($parts['2'], '2'));
+        $remove = strpos($result, ".");
+        if ($remove != false)
+        {
+            $result = substr($result, 0, strpos($result, "."));
+        }
+        return $result;
+    }
+
+    private function community_to_steamid($community)
+    {
+        if (substr($community, -1) % 2 == 0)
+            $server = 0;
+        else
+            $server = 1;
+        $auth = bcsub($community, '76561197960265728');
+        if (bccomp($auth, '0') != 1)
+        {
+            return false;
+        }
+        $auth = bcsub($auth, $server);
+        $auth = bcdiv($auth, 2);
+        return 'STEAM_0:' . $server . ':' . $auth;
+    }
+
 }
