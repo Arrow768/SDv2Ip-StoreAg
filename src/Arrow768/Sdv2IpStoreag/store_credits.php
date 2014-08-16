27bcc3e27b1560c8737b7d9cb787b5e1cdc75a1d
Werner 'Arrow768 <arrow768@sourcedonates.com>
2014-08-16 16:18:53 +0200

    Logging added

--- src/Arrow768/Sdv2IpStoreag/store_credits.php
+++ src/Arrow768/Sdv2IpStoreag/store_credits.php
@@ -17,7 +17,31 @@ class store_credits
     
     function add_item($sd_user_id,$sd_item_id)
     {
+        $item = \SDItem::find($sd_item_id);
+        if(!$item)
+        {
+            Log::Error("Could not find item with id: ".$sd_item_id);
+            exit();
+        }
         
+        $user = \Sentinel::findById($sd_user_id);
+        if(!$user)
+        {
+            Log::Error("Could not find user with id:".$sd_user_id);
+            exit();
+        }
+        
+        $userinfo_steamid = \SDUserinfo::where('type','steamid')->where('user_id',$sd_user_id)->first();
+        if(!$userinfo_steamid)
+        {
+            Log::Error("Could not find Steamid for User");
+            exit();
+        }
+        
+        Log::Debug("Got Required info");
+        Log::Debug("Item Name:".$item->name_short);
+        Log::Debug("User Mail:".$user->email);
+        Log::Debug("SteamID64:".$userinfo_steamid->value);
     }
     
     function remove_item($sd_user_id,$sd_item_id)
